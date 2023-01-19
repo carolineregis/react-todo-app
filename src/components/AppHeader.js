4d153373455ccd79ee167f87853c4c87f1584b87
Carol <caroline.regis@devmagic.com.br>
2023-01-19 16:37:25 -0300

    WIP(*): adicionando estado aos botões do modal

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button, {SelectButton} from './Button'
 import styles from '../styles/modules/app.module.scss'
 import { useDispatch, useSelector } from 'react-redux';
 import ToDoModal from './ToDoModal';
 
 function AppHeader() {
-  const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(true);
 
   return (
     <div className={styles.appHeader}>
-      <Button variant='primary'>
+      <Button variant='primary' onClick={() => setModalOpen(true)}>
         Add Task
       </Button>
 
@@ -19,7 +19,7 @@ function AppHeader() {
         <option value="complete"> Complete </option>
       </SelectButton>
 
-      <ToDoModal></ToDoModal>
+      <ToDoModal modalOpen={modalOpen} setModalOpen={setModalOpen}></ToDoModal>
     </div>
   );
 }
--- src/components/ToDoModal.js
+++ src/components/ToDoModal.js
@@ -1,35 +1,70 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from '../styles/modules/modal.module.scss'
 import {MdOutlineClose} from 'react-icons/md'
 import Button from './Button';
 
-function ToDoModal() {
+
+function ToDoModal({modalOpen, setModalOpen}) {
+  const [title, setTitle] = useState('');
+  const [status, setStatus] = useState('incomplete');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({title, status});
+  }
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.closeButton}>
-          <MdOutlineClose></MdOutlineClose>
+
+      modalOpen && (
+        <div className={styles.wrapper}>
+          <div className={styles.container}>
+            <div 
+              className={styles.closeButton}
+              onClick={() => setModalOpen(false)}
+              onKeyDown={() => setModalOpen(false)}
+              tabIndex={0}
+              role="button"
+            >
+              <MdOutlineClose></MdOutlineClose>
+            </div>
+            <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+              <h1 className={styles.formTitle}> Add Task </h1>
+              <label htmlFor='title'>
+                Title
+                <input 
+                  type='text' 
+                  id='title' 
+                  value={title}
+                  onChange={(e) =>setTitle(e.target.value)}
+                />
+              </label>
+              <label htmlFor='status'>
+                Status
+                <select 
+                  name='status' 
+                  id='status'
+                  value={status}
+                  onChange={(e) => setStatus(e.target.value)}
+                >
+                  <option value='incomplete'> Incomplete </option>
+                  <option value='complete'> Complete </option>
+                </select>
+              </label>
+              <div className={styles.buttonContainer}>
+                <Button 
+                  type='submit' 
+                  variant='primary'
+                > Add Task</Button>
+                <Button 
+                  type='button' 
+                  variant='secondary'
+                  onClick={() => setModalOpen(false)}
+                  onKeyDown={() => setModalOpen(false)}
+                > Cancel</Button>
+              </div>
+            </form>
+          </div>  
         </div>
-        <form className={styles.form}>
-          <h1 className={styles.formTitle}> Add Task </h1>
-          <label htmlFor='title'>
-            Title
-            <input type='text' id='title'/>
-          </label>
-          <label htmlFor='status'>
-            Status
-            <select name='status' id='status'>
-              <option value='incomplete'> Incomplete </option>
-              <option value='complete'> Complete </option>
-            </select>
-          </label>
-          <div className={styles.buttonContainer}>
-            <Button type='submit' variant='primary'> Add Task</Button>
-            <Button type='button' variant='secondary'> Cancel</Button>
-          </div>
-        </form>
-      </div>  
-    </div>
+      )
   );
 }
 
